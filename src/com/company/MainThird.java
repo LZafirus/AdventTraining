74c70aca0487ec169232bad054ceb8cddb277e8b
Dawid <walaszczyk.dawid@gmail.com>
2021-12-04 11:20:36 +0100

    First puzzle, third day answer

--- src/com/company/MainThird.java
+++ src/com/company/MainThird.java
@@ -9,9 +9,6 @@ import java.util.Scanner;
 public class MainThird {
     public static void main(String[] args) throws FileNotFoundException {
 
-        int gammaRate = 0;
-        int epsilonRate = 0;
-
         File file = new File("input3.txt");
         Scanner scanner = new Scanner(file);
         List<String> input = new ArrayList<>();
@@ -21,20 +18,35 @@ public class MainThird {
             input.add(value);
         }
 
-        int tracker = 0;
-        String result = "";
+        String gamma = "";
+        String epsilon = "";
+
         int zeros = 0;
         int ones = 0;
 
-        for (int i = 0; i < input.size(); i++) {
-            if(input.get(i).substring(0,1).equals("0")){
-                zeros++;
-            }
-            else {
-                ones ++;
-            }
-        }
-        System.out.println("Zeros: " + zeros+ ", ones: " + ones);
+        int sizeInput = input.get(1).length();
 
+        for (int j = 0; j < sizeInput; j++) {
+            for (int i = 0; i < input.size(); i++) {
+                if (input.get(i).substring(j, j+1).equals("0")) {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+            }
+            System.out.println( j+ " rządek - " + "Zeros: " + zeros + ", ones: " + ones);
+            if(zeros > ones) {
+                gamma += "0";
+                epsilon += "1";
+            } else {
+                gamma += "1";
+                epsilon +="0";
+            }
+            zeros = 0;
+            ones = 0;
+        }
+        int gammaInt = Integer.parseInt(gamma, 2);
+        int epsilonInt = Integer.parseInt(epsilon, 2);
+        System.out.println(gammaInt * epsilonInt);
     }
 }
