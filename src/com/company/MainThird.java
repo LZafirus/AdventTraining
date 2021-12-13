4df983ad2d650101c2fa91d63c670216aab46a36
Dawid <walaszczyk.dawid@gmail.com>
2021-12-13 09:44:35 +0100

    third puzzle - second part

--- src/com/company/MainThird.java
+++ src/com/company/MainThird.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,6 +14,7 @@ public class MainThird {
         File file = new File("input3.txt");
         Scanner scanner = new Scanner(file);
         ArrayList<String> input = new ArrayList<>();
+        ArrayList<String> output = new ArrayList<>();
 
         while(scanner.hasNext()) {
             String value = scanner.next();
@@ -24,37 +27,42 @@ public class MainThird {
         int zeros = 0;
         int ones = 0;
 
-        int sizeInput = 12;
-
-        for (int j = 0; j < input.size(); j++) {
-            for (int i = 0; i < input.size(); i++) {
+        for (int j = 0; j < 9; j++) {
+            int inputSize = input.size();
+            for (int i = 0; i < inputSize; i++) {
                 if (input.get(i).substring(j, j+1).equals("0")) {
                     zeros++;
                 } else {
                     ones++;
                 }
             }
-            System.out.println("zeros: " + zeros + ", ones: " + ones);
+            System.out.println("Input size: " + inputSize + "zeros: " + zeros + ", ones: " + ones);
 
-            if ( ones > zeros ) {
-                for (int k = 0; k < input.size(); k++) {
-                    if (input.get(k).substring(j, j+1).equals("0")) input.remove(k);
-                }
-            } if (zeros >= ones ) {
-                for (int l = 0; l < input.size(); l++) {
-                    if (input.get(l).substring(j, j+1).equals("1")) input.remove(l);
-                }
-            } if (zeros == ones) {
-                for (int d = 0; d < input.size(); d++) {
-                    if (input.get(d).substring(j, j+1).equals("0")) input.remove(d);
+            if ( zeros > ones) {
+                for (int i = 0; i < inputSize; i++) {
+                    if(input.get(i).substring(j, j+1).equals("1")) output.add(input.get(i));
                 }
             }
+            if (ones >= zeros) {
+                for (int i = 0; i < inputSize; i++) {
+                    if(input.get(i).substring(j, j+1).equals("0")) output.add(input.get(i));
+                }
+            }
+            if(inputSize == 1) System.out.println(input.get(0));
 
-            System.out.println(j);
+            input.removeAll(input);
+            input.addAll(output);
+            output.removeAll(output);
+
             zeros = 0;
             ones = 0;
 
         }
+
+        int oxygenRating = Integer.parseInt("111000100010", 2); //931 first, 3618 second
+        System.out.println(931 *3618);
+    }
+}
 
 
 
@@ -85,6 +93,3 @@ public class MainThird {
 
 */
 
-        
-    }
-}
